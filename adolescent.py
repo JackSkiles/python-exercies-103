2a32eb59b985da909a4f2a7d1e4d7ea473859e19
Jack Skiles <jackskiles420@gmail.com>
2020-04-02 09:39:52 -0400

    adolescent.py file

---
+++ adolescent.py
@@ -0,0 +1,10 @@
+
+#creates answer variable
+
+answer = ' '
+
+#creates a while loop that will infinitely loop unitil the answer givin is yes.
+while answer != "yes":
+    answer = input('But I really want to! Can I?!')
+    answer = answer.lower()
+print('Thanks!')
