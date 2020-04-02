0f52695c9502734bd6cdb8f13e9e79118ba529dd
Jack Skiles <jackskiles420@gmail.com>
2020-04-02 09:47:45 -0400

    adds counter.py

---
+++ counter.py
@@ -0,0 +1,8 @@
+
+#creates a counter to interate on/
+count = 0
+
+#creates a while loop that interates counter by + 1 as long as it is less than or equal to 10. After 10 it quits.
+while count <= 10:
+    print(count)
+    count = count + 1
